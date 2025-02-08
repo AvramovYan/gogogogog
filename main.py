47393111450ef29a4f2230d848c108c858a34e76
Aleksey <mikheydazz2020@gmail.com>
2025-02-08 18:54:32 +0700

    memes and joke show commands

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 
 import telebot
+from telebot import types
+from Mikheev import Mikheev, answer
 bot = telebot.TeleBot("7024653639:AAFxfsxImjJSj-mtkNEeuIdnu8I6OftepbI")
 @bot.message_handler(commands=["help","start"])
 def info(message):
     bot.send_message(message.chat.id,"startlox")
+
+@bot.message_handler(commands=['mikheev'])
+def memes(message):
+    Mikheev(message, bot, types)
+
+@bot.callback_query_handler(func=lambda call: True)
+def show_meme_or_joke(call):
+    if call.message:
+        answer(call, bot, types)
 
