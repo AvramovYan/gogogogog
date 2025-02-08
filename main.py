bc12c071e4c71cfb24b1c3a7c910fe2ee36f801a
g74gq <nekorsakova2309@gmail.com>
2025-02-08 17:17:32 +0300

    ну дa

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 
 import telebot
+
 bot = telebot.TeleBot("7024653639:AAFxfsxImjJSj-mtkNEeuIdnu8I6OftepbI")
 @bot.message_handler(commands=["help","start"])
 def info(message):
     bot.send_message(message.chat.id,"startlox")
 
+
+@bot.message_handler(commands=["whatch"])
+def lala(message):
+    lalala(message, bot, types)
+
+@bot.message_handler(content_types=["text"])
+def kors(message):
+    korsak(message, bot, types)
