912ada5a4c6ecf4763aa1219fa9f192dcbb266de
oboyudniy <revete9765@gmail.com>
2025-02-08 19:28:21 +0700

    ds

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 import telebot
+from pomacckibidi import pomacckibidi
 bot = telebot.TeleBot("7024653639:AAFxfsxImjJSj-mtkNEeuIdnu8I6OftepbI")
 @bot.message_handler(commands=["help","start"])
 def info(message):
     bot.send_message(message.chat.id,"startlox")
 
+@bot.message_handler(commands=["help","start"])
+def pomacckibidi(message):
+    pomacckibidi(message, bot, types)
