bcaf86a9f69a89b805166d2b0d5585fca909286a
AndreyTech <lionlonn14@gmail.com>
2025-02-08 15:36:20 +0300

    feature(master): add new handler teach in main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 
 import telebot
+from telebot import types
+from teacher import teacher
 bot = telebot.TeleBot("7024653639:AAFxfsxImjJSj-mtkNEeuIdnu8I6OftepbI")
 @bot.message_handler(commands=["help","start"])
 def info(message):
     bot.send_message(message.chat.id,"startlox")
 
+
+@bot.message_handler(commands=["teacher"])
+def teach(message):
+    teacher(message, bot, types)
+
+
+
+
+
+
+bot.infinity_poling()
