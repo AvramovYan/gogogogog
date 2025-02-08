0708da2043f3ed8e67a3eaa528627d423c20b3b6
ya lox <152689259+AvramovYan@users.noreply.github.com>
2025-02-08 19:04:07 +0700

    wgidf

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-
+admins = [2073232860]
 import telebot
 bot = telebot.TeleBot("7024653639:AAFxfsxImjJSj-mtkNEeuIdnu8I6OftepbI")
 @bot.message_handler(commands=["help","start"])
 def info(message):
-    bot.send_message(message.chat.id,"startlox")
-
+    user_id = message.chat.id
+    if user_id in admins:
+        bot.send_message(message.chat.id, "ты че АДМИН?!?!?!?")
+    else:
+        bot.send_message(message.chat.id,"startlox")
